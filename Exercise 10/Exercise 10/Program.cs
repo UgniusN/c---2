17b038b102bf0801b25e39bd7f2e48751196a725
Ugnius <ugniusnaujokas@gmail.com>
2019-12-17 21:04:53 +0200

    Completed Exercise 10

---
+++ Exercise 10/Exercise 10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite spalva");
+            String spalva = Console.ReadLine();
+            if((spalva == "geltona") || (spalva == "zalia") || (spalva == "raudona"))
+            {
+                Console.WriteLine("{0} - tai spalva is trispalves", spalva);
+            }
+            else {
+                Console.WriteLine("{0} - spalva nepriklauso trispalvei", spalva);
+            }
+            Console.Read();
+        }
+    }
+}
