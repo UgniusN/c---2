3684dd8add43c9f3a1a24922898d0e1bb8267227
Ugnius <ugniusnaujokas@gmail.com>
2019-12-17 20:57:20 +0200

    Completed Exercise 9

---
+++ Exercise 9/Exercise 9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite grupes pavadinima: ");
+            string grupesPavadinimas = Console.ReadLine();
+
+            Console.WriteLine("Iveskite grupes nariu skaiciu: ");
+            int nariuSkaicius = 0;
+
+            if (!int.TryParse(Console.ReadLine(), out nariuSkaicius))
+            {
+                Console.WriteLine("Ivestas neteisingas skaicius");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.WriteLine("Grupes pavadinimas yra: {0}, nariu skaicius yra {1}", grupesPavadinimas, nariuSkaicius);
+            Console.ReadLine();
+        }
+    }
+}
