bb402da93597a53d591fe9a499c2c2ecc507dfa9
Ugnius <ugniusnaujokas@gmail.com>
2019-12-17 20:29:31 +0200

    Completed Exercise 7

---
+++ Exercise 7/Exercise 7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite temperatura(C): ");
+            String a = Console.ReadLine();
+            int C = Convert.ToInt32(a);
+            double K = (double)C * 274.15;
+            double F = (double)C * 33.8;
+            Console.WriteLine("{0} Celciju yra {1} Kelvinu",C,K);
+            Console.WriteLine("{0} Celciju yra {1} Farenhaitu", C, F);
+            Console.ReadLine();
+        }
+    }
+}
