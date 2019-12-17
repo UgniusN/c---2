b31799153222eef682b5f54b2e9cc15d235e5737
Ugnius <ugniusnaujokas@gmail.com>
2019-12-17 20:03:04 +0200

    Completed exercise 4

---
+++ Exercise 4/Exercise 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String a = Console.ReadLine();
+            double b = Convert.ToDouble(a);
+            String c = Console.ReadLine();
+            double d = Convert.ToDouble(c);
+            var e = ((b % 2 == 0) == (d % 2 == 0));
+            Console.WriteLine(e);
+            Console.ReadLine();
+        }
+    }
+}
