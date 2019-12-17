bd153d37f471779d6fd1266b557041c8e91c9bf0
Ugnius <ugniusnaujokas@gmail.com>
2019-12-17 19:09:21 +0200

    Completed ConsoleApp3

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -6,10 +6,25 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp3
 {
+    class Human
+    {
+        public string Name;
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            Human human1 = new Human();
+            human1.Name = "Batman";
+            Human human2 = human1;
+
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+
+            human2.Name = "Joker";
+            Console.WriteLine("human1.Name: " + human1.Name);
+            Console.WriteLine("human2.Name: " + human2.Name);
+            Console.ReadLine();
         }
     }
 }
