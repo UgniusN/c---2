271f65766e0371263c4dbabb24fa898190f41d78
Ugnius <ugniusnaujokas@gmail.com>
2019-12-17 20:24:47 +0200

    Completed Exercise_6

--- Exercise 6/Exercise 6/Program.cs
+++ Exercise 6/Exercise 6/Program.cs
@@ -10,6 +10,35 @@ namespace Exercise_6
     {
         static void Main(string[] args)
         {
+            String user;
+            String pass;
+            bool busena = false;
+            for (int i=0; i<3; i++)
+            {
+                Console.WriteLine("Iveskite prisijungimo varda: ");
+                user = Console.ReadLine();
+                Console.WriteLine("Iveskite prisijungimo slaptazodi: ");
+                pass = Console.ReadLine();
+                if (user == "rimas" && pass == "kietas")
+                {
+                    busena = true;
+                    break;
+                }
+                else
+                {
+                    busena = false;
+                    Console.WriteLine("Neteisingas slaptazodis arba prisijungimo vardas");
+                }
+            }
+            if (busena == true)
+            {
+                Console.WriteLine("Sekmingai prisijungta");
+            }
+            else
+            {
+                Console.WriteLine("Virsijote prisijungimo limita");
+            }
+            Console.ReadLine();
         }
     }
 }
